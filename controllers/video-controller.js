2b19ae195efa4647a3bc01be54f1bbfd694e7fd2
Xenrken <xenraken@hotmail.com>
2025-03-18 22:21:07 +0300

    add video delete method

--- controllers/video-controller.js
+++ controllers/video-controller.js
@@ -1,4 +1,6 @@
 const { dbInsertVideoRecord } = require("../db/db-operations");
+const fs = require("fs");
+const path = require("path");
 
 // upload the given video file to db
 async function videoUpload(req, res)
@@ -14,7 +16,7 @@ async function videoUpload(req, res)
         }
         if (!videoFile)
         {
-            return res.status(400).json({ message: "Video file not found" });
+            return res.status(400).json({ message: "File not found" });
         }
         const record =
         {
@@ -23,20 +25,51 @@ async function videoUpload(req, res)
         }
 
         await dbInsertVideoRecord("videos", record);
-        return res.status(201).json({ message: "Video upload is successful", record });
+        return res.status(201).json({ message: "File upload is successful", record });
     }
     catch (err) 
     {
         console.error("Video upload error:", err);
-        return res.status(500).json({ message: "An error occurred during video upload", error: err.message });
+        return res.status(500).json({ message: "An error occurred during file upload", error: err.message });
     }
 }
 
+// delete the videofile and correresponding db record
+async function videoDelete(req, res) 
+{
+    try 
+    {
+        console.log("Current working directory:", process.cwd());
+        const filename = req.params.filename;
+        const filePath = path.join(__dirname, "../uploads", filename);
+        if (!fs.existsSync(filePath)) 
+        {
+            console.error("File not found")
+            return res.status(404).json({ message: "File not found" });
+        }
 
+        fs.unlink(filePath, (err) =>
+        {
+            if (err)
+            {
+                console.error("Error deleting file:", err);
+                return res.status(500).json({ message: "Error deleting file" });
+            }
 
+            res.status(200).json({ message: "File deleted successfully" });
+        });
+
+    } catch (err) 
+    {
+        console.error("Video deleting error:", err);
+        return res.status(500).json({ message: "An error occurred during video upload", error: err.message });
+    }
+
+}
 
 
 module.exports =
 {
     videoUpload,
+    videoDelete,
 }
--- routers/videos.js
+++ routers/videos.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { upload } = require("../middlewares/upload");
-const { videoUpload, } = require("../controllers/video-controller");
+const { videoUpload, videoDelete } = require("../controllers/video-controller");
 const { verifyToken } = require("../middlewares/authentication");
 
 
 router.post("/upload", verifyToken, upload, videoUpload);
+
+router.delete("/:filename", videoDelete);
 
 
 module.exports = router;
